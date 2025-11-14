ec5ecf8f44e0de45678b1f25b6cad3ea253c23d4
gauravK0i <gaurav.koirala@globalsquareit.com>
2025-11-14 10:40:59 +0545

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using GenerativeAI;
+
+namespace NepaliInvestmentAdvisor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string apiKey = "AIzaSyA3_UeAEtKc5GlIV0fHeWrIITmI6sGiJIE";
+
+            var client = new GenerativeModel(apiKey ,"gemini-2.5-pro");
+
+            // Example user data
+            // double income = 50000;
+            // double expenses = 35000;
+            // string riskTolerance = "medium";
+            // string goal = "long-term growth";
+            string prompt = "Giv me 4 ideas for Final Year Projects";
+            Console.Write("Hello");
+            var response = await client.GenerateContentAsync(prompt);
+            Console.WriteLine(response.Text);
+        }
+    }
+}
