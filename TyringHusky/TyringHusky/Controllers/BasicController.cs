bcc35398b7833a722289975203ccffa7d78f56f6
Gaurav-Koirala-Herald <np03cs4a230112@heraldcollege.edu.np>
2025-12-18 13:11:35 +0545

    New project

---
+++ TyringHusky/TyringHusky/Controllers/BasicController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TyringHusky.Controllers;
+
+[ApiController]
+public class BasicController : ControllerBase
+{
+    public IActionResult GetHealth()
+    {
+        return Ok("Good");
+    }
+}
